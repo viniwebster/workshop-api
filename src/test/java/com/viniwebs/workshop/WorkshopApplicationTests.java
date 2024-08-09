100b46be1597bd05b5abb9280cb6b5837514ccf7
viniwebster <viniciuswebster99@gmail.com>
2024-08-08 22:09:54 -0300

    creating project springboot and mongoDB

---
+++ src/test/java/com/viniwebs/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniwebs.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
